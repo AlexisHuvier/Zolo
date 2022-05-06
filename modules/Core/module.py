048a80e0d268d73d82a16c2cb631808e00f85231
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-06 18:21:31 +0200

    Add dismodule, actmodule and modules commands in Core module

--- modules/Core/module.py
+++ modules/Core/module.py
@@ -5,3 +5,24 @@ class Core:
     def stop(zolo, module, args):
         zolo.stop()
 
+    def dismodule(zolo, module, args):
+        if len(args) == 1:
+            zolo.desactive_module(args[0])
+            print(f"[Core] Module {args[0]} désactivé")
+        else:
+            print("[ERREUR] Syntaxe : core dismodule <module>")
+    
+    def actmodule(zolo, module, args):
+        if len(args) == 1:
+            zolo.active_module(args[0])
+            print(f"[Core] Module {args[0]} activé")
+        else:
+            print("[ERREUR] Syntaxe : core actmodule <module>")
+
+    def modules(zolo, module, args):
+        print("[Core] Liste des modules :")
+        for i in zolo.modules:
+            if i.name in zolo.config.disabled_modules:
+                print(f"[Core] - {i.name} (désactivé)")
+            else:
+                print(f"[Core] - {i.name}")
