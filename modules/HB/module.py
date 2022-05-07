6c195e4db004bab0a8c887caaad21d225fb5c3e9
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-08 00:22:19 +0200

    Create HB module

---
+++ modules/HB/module.py
@@ -0,0 +1,53 @@
+from urllib import response
+import bs4
+import requests
+import time
+from modules.HB import parser_hb
+
+
+class HB:
+    def info(zolo, module, args):
+        """
+        Info about HB module
+
+        Args:
+            zolo (Zolo): Zolo
+            module (Module): Current Module
+            args (list(string)): List of arguments of command
+        """
+        print(f"[HB] Version {module.version}")
+        
+    def perso(zolo, module, args):
+        """
+        Get Informations about personnage
+
+        Args:
+            zolo (Zolo): Zolo
+            module (Module): Current Module
+            args (list(string)): List of arguments of command
+        """
+        if args:
+            perso = args[0]
+            account = open("modules/HB/account.txt", "r").readlines()
+            start = time.time()
+            response = parser_hb.connection(account[0].strip(), account[1].strip())
+            print(f"[HB] Connexion réussie (Temps : {(time.time() - start):.2f}s)")
+            
+            start = time.time()
+            player = parser_hb.get_infos_for(perso, response.cookies)
+            if player:
+                print(f"[HB] Informations recueillies pour {player[0]} ({player[2]}) (Temps : {(time.time() - start):.2f}s) :")
+                print(f"     - Pseudo MC : {player[1]}")
+                print(f"     - Titre : {player[3]}")
+                print(f"     - Genre : {player[4]}")
+                print(f"     - Status du BG : {player[5]}")
+                print(f"     - Nombre de message sur le forum : {player[6]}")
+                print(f"     - Pourcentage de quêtes réussies : {player[7]}")
+                print(f"     - Nombre de mantra : {player[8]}")
+                print(f"     - Nombre de fragments : {player[9]}")
+                print(f"     - Activité : {player[10]}")
+                print(f"     - Nombre d'ampoules : {player[11]} ({player[12]} allumée(s), {player[13]} éteinte(s), {player[14]} cassée(s))")
+            else:
+                print(f"[ERREUR] Personnage inconnu (Temps : {(time.time() - start):.2f}s)")
+        else:
+            print("[ERREUR] Syntaxe : hb perso <perso>")
---
+++ modules/HB/parser_hb.py
@@ -0,0 +1,39 @@
+import requests
+import bs4
+
+def request(page, cookies = None):
+    return requests.get(page, cookies = cookies) if cookies else requests.get(page)
+
+def get_infos_for(user, cookies, nb_cellules_per_user = 13):
+    soup = bs4.BeautifulSoup(request("https://www.herobrine.fr/index.php?p=members&search="+user, cookies).text, "html5lib")
+    temp = soup.find(id="main").find_all("td")
+    for i in range(len(temp) // nb_cellules_per_user):
+        if temp[nb_cellules_per_user*(i-1)].a.text.strip() == user:
+            ampoules = [0, 0, 0]
+            for img in temp[nb_cellules_per_user*(i-1) + 11].find_all("img"):
+                if "lamp1" in img["src"]:
+                    ampoules[0] += 1
+                elif "lamp0" in img["src"]:
+                    ampoules[1] += 1
+                elif "lampnull" in img["src"]:
+                    ampoules[2] += 1
+            return [
+                temp[nb_cellules_per_user*(i-1)].a.text.strip(), 
+                temp[nb_cellules_per_user*(i-1)].a["title"].split(" ")[-1].strip(),
+                temp[nb_cellules_per_user*(i-1)].a["href"].split("=")[-1].strip(),
+                temp[nb_cellules_per_user*(i-1) +1].text.strip(),
+                temp[nb_cellules_per_user*(i-1) +2].img["title"].strip(), 
+                "Pas de BG" if temp[nb_cellules_per_user*(i-1) +3].img is None else temp[nb_cellules_per_user*(i-1) +3].img["title"].strip(), 
+                temp[nb_cellules_per_user*(i-1) +4].text.strip(), 
+                temp[nb_cellules_per_user*(i-1) +5].text.strip(),
+                temp[nb_cellules_per_user*(i-1) +6].text.strip(), 
+                "0" if temp[nb_cellules_per_user*(i-1) +7].text == "" else temp[nb_cellules_per_user*(i-1) +7].text.strip(),
+                temp[nb_cellules_per_user*(i-1) +10].img["title"].strip(), 
+                sum(ampoules), 
+                *ampoules
+                , temp[nb_cellules_per_user*(i-1)].a["href"].split("=")[-1].strip(),
+            ]
+
+def connection(email, mdp):
+    payload = {'email': email, 'pass': mdp, 'login': '1'}
+    return requests.post("https://www.herobrine.fr/index.php?p=login", data=payload)
