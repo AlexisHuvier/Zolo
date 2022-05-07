0543d40beea8532b4fb2e1ad36c0981e4d3662d1
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-05-08 00:22:31 +0200

    Create Meteofrance module

---
+++ modules/MeteoFrance/module.py
@@ -0,0 +1,74 @@
+import webbrowser
+from meteofrance_api import MeteoFranceClient
+import datetime
+
+
+class MeteoUtilities:
+    def print_forecast(forecast):
+        dt = datetime.datetime.fromtimestamp(forecast["dt"])
+        sun_rise = datetime.datetime.fromtimestamp(forecast["sun"]["rise"])
+        sun_set = datetime.datetime.fromtimestamp(forecast["sun"]["set"])
+        print(f"[Meteo] Information pour le {dt.strftime('%d/%m/%Y à %H:%M')} :")
+        print(f"        - Température : Min {forecast['T']['min']}°C - Max {forecast['T']['max']}°C")
+        print(f"        - Humidité : Min {forecast['humidity']['min']}% - Max {forecast['humidity']['max']}%")
+        print(f"        - Précipitations : {forecast['precipitation']['24h']}mm")
+        print(f"        - Soleil : Levé à {sun_rise.strftime('%H:%M')} - Couché à {sun_set.strftime('%H:%M')} - UV : {forecast['uv'] if forecast['uv'] else 'Inconnu'}")
+        print(f"        - Appréciation : {forecast['weather12H']['desc']}")
+
+class MeteoFrance:
+    client = MeteoFranceClient()
+    
+    def info(zolo, module, args):
+        """
+        Info about MeteoFrance module
+
+        Args:
+            zolo (Zolo): Zolo
+            module (Module): Current Module
+            args (list(string)): List of arguments of command
+        """
+        print(f"[Meteo] Version {module.version}")
+        
+    def picture(zolo, module, args):
+        """
+        Get Picture of the Day
+
+        Args:
+            zolo (Zolo): Zolo
+            module (Module): Current Module
+            args (list(string)): List of arguments of command
+        """
+        potd = MeteoFrance.client.get_picture_of_the_day()
+        print(f"[Meteo] Description PotD : {potd.description}")
+        rep = input("[Meteo] Ouvrir l'image ? (y/n) : ")
+        if rep == "y":
+            webbrowser.open(potd.image_hd_url)
+        
+        
+    def weekly(zolo, module, args):
+        """
+        Get weekly information about city
+
+        Args:
+            zolo (Zolo): Zolo
+            module (Module): Current Module
+            args (list(string)): List of arguments of command
+        """
+        place = MeteoFrance.client.search_places(" ".join(args))[0]
+        forecast = MeteoFrance.client.get_forecast_for_place(place)
+        
+        for i in forecast.daily_forecast:
+            MeteoUtilities.print_forecast(i)
+            
+    def daily(zolo, module, args):
+        """
+        Get daily information about city
+
+        Args:
+            zolo (Zolo): Zolo
+            module (Module): Current Module
+            args (list(string)): List of arguments of command
+        """
+        place = MeteoFrance.client.search_places(" ".join(args))[0]
+        forecast = MeteoFrance.client.get_forecast_for_place(place)
+        MeteoUtilities.print_forecast(forecast.today_forecast)
